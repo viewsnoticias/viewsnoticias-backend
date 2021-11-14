1800d67157d03a479332a59cf7f25b8a00b2262c
luis yegre <luis_yegre@hotmail.com>
2021-11-13 22:09:45 -0400

    updating

--- start/routes.ts
+++ start/routes.ts
@@ -6,9 +6,18 @@ Route.get('/', async () => {
 
 Route.post('api/v1/auth/login','AuthController.login')
 
+
+//rutas para obtener noticias y secciones
+Route.group(()=> {
+  Route.get('/news','NewsController.index')
+  Route.get('/news/:id','NewsController.show')
+  Route.get('/sections','SectionsController.index')
+  Route.get('/sections/:id','SectionsController.show')
+}).prefix('api/v1')
+//grupo de rutas de la api (backend)
 Route.group(()=>{
-  Route.resource('/news','NewsController').except(['edit','create'])
-  Route.resource('/sections','SectionsController').except(['edit','create'])
+  Route.resource('/news','NewsController').except(['edit','create','show','index'])
+  Route.resource('/sections','SectionsController').except(['edit','create','show','index'])
   Route.resource('/users','UsersController').except(['edit','create'])
   Route.get('/my-news/:id','UsersController.myNews')
   Route.resource('/roles','RolesController').except(['edit','create'])
