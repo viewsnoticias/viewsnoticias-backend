900e6b4825250dea1b67c6c5ce2bb68031017718
luis yegre <luis_yegre@hotmail.com>
2021-11-18 21:10:40 -0400

    conf deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps : [{
+    name: "vn-backend",
+    script: "npm run build;",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+      PORT:3333,
+      APP_KEY:"oAXohJmp8-ebwrm8HJE-z25myhSqqNma",
+      DB_CONNECTION:"sqlite"
+    }
+  }, {
+     name: 'worker',
+     script: 'worker.js'
+  }]
+}
